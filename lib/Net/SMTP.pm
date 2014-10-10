2c160b441ccc609bf4397ad7dd783c68b8cac4c2
Steve Hay <steve.m.hay@googlemail.com>
2014-10-10 23:42:22 +0100

    Fix $smtp->auth($sasl)
    Try the AUTH mechanism (if present) in the Authen::SASL object before
    falling back on other mechanisms.
    
    Resolves CPAN RT#99415.

--- lib/Net/SMTP.pm
+++ lib/Net/SMTP.pm
@@ -193,8 +193,8 @@ sub auth {
         if $self->debug;
       $mechanisms =~ s/\b\Q$failed_mechanism\E\b//;
       last unless $mechanisms =~ /\S/;
+      $sasl->mechanism($mechanisms);
     }
-    $sasl->mechanism($mechanisms);
     
     # We should probably allow the user to pass the host, but I don't
     # currently know and SASL mechanisms that are used by smtp that need it
